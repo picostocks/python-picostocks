cb1cd2c1e1979c0ba40a61b22bc528e691ee520a
Krzysztof Kuchta <kkuchta@togetherdata.com>
2018-04-19 20:45:39 +0200

    Fixing get_stocks endpoint.

--- picostocks/client.py
+++ picostocks/client.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from picostocks.utils import decimal2string
+from .utils import decimal2string
 
 import asyncio
 import urllib.parse
@@ -10,49 +10,46 @@ import requests
 
 
 class Exchanger(object):
-    PREFIX_URL = "http://localhost:8000/api/"
+    PREFIX_URL = "https://picostocks.com/api/"
 
     def __init__(self, private_key, user_id):
         self.private_key = private_key
         self.user_id = user_id
         self.session = requests.Session()
         self.session.headers.update({'User-Agent': 'picostocks/python'})
-        self.signing_key = ed25519.SigningKey(self.private_key,
-                                              encoding='hex')
+        self.signing_key = ed25519.SigningKey(self.private_key, encoding='hex')
 
-    async def _get_order_request_data(self, sign_key, order_obj):
-        stock_id, price_id = order_obj.stock_id, order_obj.price_id
-        nonce = await self._get_nonce()
+    async def _get_order_request_data(self, sign_key, stock_id, price_id, quantity, price):
+        nonce_resp = await self.get_nonce()
 
         sign_message = ":".join([
             sign_key,
             str(self.user_id),
             str(stock_id),
-            decimal2string(order_obj.quantity),
+            decimal2string(quantity),
             str(price_id),
-            decimal2string(order_obj.price),
-            str(nonce)
+            decimal2string(price),
+            str(nonce_resp['nonce'])
         ])
 
         return {
             'user_id': self.user_id,
             'stock_id': stock_id,
-            'shares': order_obj.quantity,
+            'shares': quantity,
             'price_id': price_id,
-            'price': order_obj.price,
-            'signature': self.signing_key.sign(sign_message.encode(),
-                                               encoding='hex')
+            'price': price,
+            'signature': self.signing_key.sign(sign_message.encode(), encoding='hex')
         }
 
-    async def _get(self, rel_url, params=None, prefix_url=None):
+    async def _get(self, rel_url, params=None):
         loop = asyncio.get_event_loop()
-        url = urllib.parse.urljoin(prefix_url or self.PREFIX_URL, rel_url)
+        url = urllib.parse.urljoin(self.PREFIX_URL, rel_url)
         return await loop.run_in_executor(
             None, lambda: self.session.get(url, params=params))
 
-    async def _post(self, rel_url, data=None, params=None, prefix_url=None):
+    async def _post(self, rel_url, data=None, params=None):
         loop = asyncio.get_event_loop()
-        url = urllib.parse.urljoin(prefix_url or self.PREFIX_URL, rel_url)
+        url = urllib.parse.urljoin(self.PREFIX_URL, rel_url)
         return await loop.run_in_executor(
             None, lambda: self.session.post(url, data=data, params=params))
 
@@ -65,52 +62,54 @@ class Exchanger(object):
         response = await self._get("market/orderbook/", params=params)
         return response.json()
 
-    async def get_assets_balance(self):
-        response = await self._get(
-            "account/balance/%s/" % self.user_id)
+    async def get_assets_balance(self, user_id=None):
+        if user_id is None:
+            user_id = self.user_id
+
+        response = await self._get("account/balance/%s/" % user_id)
         return response.json()
 
-    async def get_open_orders(self, stock_id, price_id, limit=100):
+    async def get_open_orders(self, stock_id, price_id, user_id=None, limit=100):
+        if user_id is None:
+            user_id = self.user_id
+
         params = {
             'stock_id': stock_id,
             'price_id': price_id,
-            'user_id': self.user_id
+            'user_id':user_id
         }
-        response = await self._get("market/orderbook/", params=params)
+
+        response = await self._get("market/orderbook/", params=dict())
         return response.json()
 
-    async def put_ask(self, ask_obj):
-        request_data = await self._get_order_request_data("ASK", ask_obj)
+    async def put_ask(self, stock_id, price_id, quantity, price):
+        request_data = await self._get_order_request_data("ASK", stock_id, price_id, quantity, price)
         response = await self._post("trader/ask/put/", request_data)
         return response.json()
 
-    async def cancel_ask(self, cancel_ask_obj):
-        request_data = await self._get_order_request_data("CANCELASK",
-                                                          cancel_ask_obj)
+    async def cancel_ask(self, stock_id, price_id, quantity, price):
+        request_data = await self._get_order_request_data("CANCELASK", stock_id, price_id, quantity, price)
         response = await self._post("trader/ask/cancel/", request_data)
         return response.json()
 
-    async def put_bid(self, bid_obj):
-        request_data = await self._get_order_request_data("BID", bid_obj)
+    async def put_bid(self, stock_id, price_id, quantity, price):
+        request_data = await self._get_order_request_data("BID", stock_id, price_id, quantity, price)
         response = await self._post("trader/bid/put/", request_data)
         return response.json()
 
-    async def cancel_bid(self, bid_obj):
-        request_data = await self._get_order_request_data("CANCELBID", bid_obj)
+    async def cancel_bid(self, stock_id, price_id, quantity, price):
+        request_data = await self._get_order_request_data("CANCELBID", stock_id, price_id, quantity, price)
         response = await self._post("trader/bid/cancel/", request_data)
         return response.json()
 
     async def get_stocks(self):
-        response = await self._get("account/transfers/external/123/52/")
+        response = await self._get("market/stocks/")
         return response.json()
+
 
 if __name__ == '__main__':
     # Example usage
-    exchanger = Exchanger(private_key=b'1246b84985e1ab5f83f4ec2bdf271114666fd3d9e24d12981a3c861b9ed523c6',
-                          user_id='1231231231')
+    exchanger = Exchanger(private_key=b'<private_key>', user_id='<user_id>')
     loop = asyncio.get_event_loop()
-    open_orders_resp = loop.run_until_complete(
-        exchanger.get_stocks())
+    open_orders_resp = loop.run_until_complete(exchanger.get_stocks())
     print(open_orders_resp)
-
-

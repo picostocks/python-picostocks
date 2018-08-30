ef1d6a3f0723c874d07a74822177701255330662
Damian Peszko <d.peszko@bioinfo.pl>
2018-08-30 14:39:04 +0200

    Exchanger now takes extra session_cls init param.

--- picostocks/client.py
+++ picostocks/client.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from picostocks.utils import float2string
-
 import asyncio
 import urllib.parse
 
 import ed25519
 import requests
 
+from picostocks.utils import float2string
+
 
 class Exchanger(object):
     PREFIX_URL = "https://picostocks.com/api/v1/"
 
-    def __init__(self, private_key, user_id):
+    def __init__(self, private_key, user_id, session_cls=None):
         self.user_id = user_id
 
         # Bytes of private key.
@@ -21,9 +21,10 @@ class Exchanger(object):
             self.private_key = self.private_key.encode()
 
         self.signing_key = ed25519.SigningKey(self.private_key, encoding='hex')
-
-        self.session = requests.Session()
-        self.session.headers.update({'User-Agent': 'picostocks/python'})
+        self.session = session_cls
+        if self.session is None:
+            self.session = requests.Session()
+            self.session.headers.update({'User-Agent': 'picostocks/python'})
 
     async def _get_order_request_data(self, sign_key, stock_id, unit_id, quantity,
                                       price):
